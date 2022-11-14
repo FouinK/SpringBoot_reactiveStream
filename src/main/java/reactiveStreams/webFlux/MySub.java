a0acb1a5563ff23f898d7ad58ba65ba3d4e9b6ff
Fouink <yyss9103@gmail.com>
2022-11-14 15:58:19 +0900

    구독 시스템 간단 구현

--- src/main/java/reactiveStreams/webFlux/MySub.java
+++ src/main/java/reactiveStreams/webFlux/MySub.java
@@ -4,14 +4,19 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 public class MySub implements Subscriber<Integer> {
+
+    private Subscription s;
     @Override
     public void onSubscribe(Subscription s) {
-        System.out.println();
+        System.out.println("구독자 : 구독정보 잘 받았어");
+        this.s = s;
+        System.out.println("구독자 : 나 이제 신문 1개씩 줘");
+        s.request(12);       //신문 한개씩 매일 매일 줘
     }
 
     @Override
     public void onNext(Integer integer) {
-        System.out.println("구독 데이터 전달");
+        System.out.println("onNext() : "+integer);
     }
 
     @Override
@@ -23,7 +28,6 @@ public class MySub implements Subscriber<Integer> {
     public void onComplete() {
         System.out.println("구독 완료");
     }
-
 
 
 }
--- src/main/java/reactiveStreams/webFlux/WebFluxApplication.java
+++ src/main/java/reactiveStreams/webFlux/WebFluxApplication.java
@@ -1,13 +1,14 @@
 package reactiveStreams.webFlux;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebFluxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebFluxApplication.class, args);
-	}
+		MyPub pub = new MyPub();		//신문사 생성
+		MySub sub = new MySub();		//구독자 생성
 
+		pub.subscribe(sub);
+	}
 }
