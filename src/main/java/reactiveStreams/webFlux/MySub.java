f4d64ef8f0f78ff8e4b85d9907ca7e620501a5f8
Fouink <yyss9103@gmail.com>
2022-11-14 16:10:03 +0900

    응답 데이터 조절

--- src/main/java/reactiveStreams/webFlux/MySub.java
+++ src/main/java/reactiveStreams/webFlux/MySub.java
@@ -6,17 +6,25 @@ import org.reactivestreams.Subscription;
 public class MySub implements Subscriber<Integer> {
 
     private Subscription s;
+    private int bufferSize = 3;
     @Override
     public void onSubscribe(Subscription s) {
         System.out.println("구독자 : 구독정보 잘 받았어");
         this.s = s;
         System.out.println("구독자 : 나 이제 신문 1개씩 줘");
-        s.request(12);       //신문 한개씩 매일 매일 줘
+        //신문 한개씩 매일 매일 줘 (백프레셔) 소비자가 한번에 처리할 수 있는 개수를 요청
+        s.request(bufferSize);
     }
 
     @Override
     public void onNext(Integer integer) {
         System.out.println("onNext() : "+integer);
+        bufferSize--;
+        if (bufferSize == 0) {
+            System.out.println("하루지남");
+            bufferSize = 3;
+            s.request(bufferSize);
+        }
     }
 
     @Override
--- src/main/java/reactiveStreams/webFlux/WebFluxApplication.java
+++ src/main/java/reactiveStreams/webFlux/WebFluxApplication.java
@@ -2,6 +2,10 @@ package reactiveStreams.webFlux;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * WebFlux = 단일스레드, 비동기 + Stream을 통해 백프레셔가 적용된 데이터만큼 간헐적 응답이 가능하다 + 데이터 소비가 끝나면 응답이 종료
+ * SSE 적용(Servlet, WebFlux) = 데이터 소비가 끝나도 Stream이 계속 유지
+ */
 @SpringBootApplication
 public class WebFluxApplication {
 
